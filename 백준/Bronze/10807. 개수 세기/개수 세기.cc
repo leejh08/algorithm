09bf5ddc353285f1b7863692566d0cb9ec99873a
이지훈 <163271977+leejh08@users.noreply.github.com>
2024-05-23 22:04:38 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    
+    for(int i=0; i<a; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+    int c;
+    scanf("%d",&c);
+    
+    int cnt=0;
+    for(int i=0; i<a; i++)
+    {
+        if(arr[i]==c) cnt++;
+    }
+    
+    printf("%d",cnt);
+    return 0;
+}
