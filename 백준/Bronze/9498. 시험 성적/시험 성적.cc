5a19d0b0201ab15da499839d4a9edaca6ba93378
이지훈 <163271977+leejh08@users.noreply.github.com>
2024-05-24 22:45:33 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    
+    scanf("%d",&i);
+    
+    if(i >= 90) {
+        printf("A");
+    }
+    else if(i >= 80) {
+        printf("B");
+    }
+    else if(i >= 70) {
+        printf("C");
+    }
+    else if(i >= 60) {
+        printf("D");
+    }
+    else {
+        printf("F");
+    }
+
+    return 0;
+}
